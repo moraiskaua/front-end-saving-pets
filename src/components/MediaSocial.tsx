752c9b61f39b1a34109e180951cee63e8f209703
D0uglasSantos <douglas.aquino@citechnology.com.br>
2023-09-06 22:47:17 -0300

    layout: starting component MediaSocial

--- src/components/MediaSocial.tsx
+++ src/components/MediaSocial.tsx
@@ -1,5 +1,11 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+
 const MediaSocial = () => {
-  return <div></div>;
+  return (
+    <div className="p-4">
+      <h3 className="mb-4">Siga-nos</h3>
+    </div>
+  );
 };
 
 export default MediaSocial;
