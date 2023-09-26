675027e069c093a68fd3bdfb11074af11a13150b
D0uglasSantos <douglas.aquino@citechnology.com.br>
2023-09-26 14:01:27 -0300

    layout: creating page about us

--- src/components/CardsProfile.tsx
+++ src/components/CardsProfile.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import EmailIcon from '@mui/icons-material/Email';
 interface FotoCardProps {
   foto: string;
   nome: string;
@@ -9,14 +12,35 @@ interface FotoCardProps {
 const CardProfile = ({ foto, nome, funcao }: FotoCardProps) => {
   return (
     <>
-      <div className="flex flex-col rounded-md w-60 h-64 bg-bg-yellow shadow-lg">
-        <div className="rounded-t-md">
-          <Image className="rounded-t-md" src={foto} alt="foto" width={240} height={350} />
-          {/* <div className=''>Redes Sociais</div> */}
+      <div className="flex flex-col rounded-md w-60 h-72 bg-white shadow-lg">
+        <div className="bg-bg-green rounded-t-md flex justify-center items-center mb-16">
+          <Image
+            className="rounded-full object-cover bg-bg-green p-1 -mb-12 h-32"
+            src={foto}
+            alt={nome}
+            width={120}
+            height={120}
+          />
         </div>
-        <div className="text-center text-slate-900 bg-white h-1/2 rounded-b-md pt-4">
-          <h2 className="text-lg font-medium">{nome}</h2>
-          <h4>{funcao}</h4>
+        <h2 className="text-center text-xl font-medium">{nome}</h2>
+        <h4 className="text-center text-lg font-thin mb-8">{funcao}</h4>
+        <div className="flex justify-center gap-1">
+          <InstagramIcon
+            className=" text-black border-2 border-black p-1 w-9 h-9 flex justify-center items-center rounded-full transition-transform hover:-translate-y-1 cursor-pointer hover:shadow-md
+             hover:shadow-[#cc1251] hover:bg-[#cc1251] hover:text-white hover:border-transparent"
+          />
+          <WhatsAppIcon
+            className=" text-black border-2 border-black p-1 w-9 h-9 flex justify-center items-center rounded-full transition-transform hover:-translate-y-1 cursor-pointer hover:shadow-md
+            hover:shadow-[#07db6a95] hover:bg-[#25d360] hover:text-white hover:border-transparent"
+          />
+          <GitHubIcon
+            className=" text-black border-2 border-black p-1 w-9 h-9 flex justify-center items-center rounded-full transition-transform hover:-translate-y-1 cursor-pointer hover:shadow-md
+            hover:shadow-black hover:bg-[#212121] hover:text-white hover:border-transparent"
+          />
+          <EmailIcon
+            className=" text-black border-2 border-black p-1 w-9 h-9 flex justify-center items-center rounded-full transition-transform hover:-translate-y-1 cursor-pointer hover:shadow-md
+            hover:shadow-[#9a1919] hover:bg-[#9a1919] hover:text-white hover:border-transparent"
+          />
         </div>
       </div>
     </>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,12 +5,84 @@ const About = () => {
   return (
     <>
       <Banner text="Conheça um pouco da nossa história!" title="Sobre Nós" />
-      <section className="min-h-screen p-8">
-        <div>
-
+      <section className="min-h-screen">
+        <div className="flex flex-col gap-4 p-8">
+          <h1 className="text center text-lg font-medium mb-4">
+            Bem vindo aos Saving Pets
+          </h1>
+          <p>
+            Somos apaixonados por animais e comprometidos com a missão de tornar
+            o mundo um lugar mais seguro e amoroso para todos os seres vivos de
+            quatro patas. O Saving Pets nasceu da convicção de que todos os pets
+            merecem amor, respeito e cuidado, e é nosso objetivo transformar
+            essa visão em realidade.
+          </p>
+          <p>
+            Nossa história começou com um grupo de amantes de animais que,
+            profundamente tocados pelas histórias de sofrimento e abuso que
+            tantos pets enfrentam, decidiram tomar uma posição. Nosso
+            compromisso é lutar contra o abuso, a negligência e a crueldade em
+            todas as suas formas, e para isso, contamos com a sua ajuda.
+          </p>
+          <p>
+            O Saving Pets é mais do que apenas um site; é um espaço seguro e
+            inclusivo para todos aqueles que desejam fazer a diferença na vida
+            dos pets. Acreditamos que a mudança começa com a conscientização e a
+            ação, e é por isso que oferecemos recursos valiosos para denúncias
+            de maus-tratos. Nosso objetivo é facilitar o processo de reportar
+            abusos, conectando aqueles que testemunham ou têm conhecimento de
+            maus-tratos a pets com as autoridades apropriadas e organizações de
+            resgate.
+          </p>
+          <p>
+            Nossa plataforma é baseada em princípios de empatia, compaixão e
+            justiça. Através da colaboração com organizações parceiras, educamos
+            a comunidade sobre a importância do bem-estar animal, fornecemos
+            informações úteis sobre como reconhecer e relatar maus-tratos e,
+            mais importante, oferecemos esperança aos animais que precisam de
+            uma voz.
+          </p>
+          <p>
+            Obrigado por visitar o Saving Pets e por ser parte da mudança.
+            Juntos, podemos salvar vidas e criar um mundo onde todos os pets
+            sejam amados, protegidos e respeitados.
+          </p>
+          <h3 className="font-semibold">
+            Com amor pelos animais,
+            <br />A equipe do Saving Pets
+          </h3>
         </div>
-        <div className='flex justify-center items-center gap-4'>
-        
+        <div className="bg-bg-yellow min-h-screen p-8 flex flex-col gap-8 items-center justify-center">
+          <h1 className="text-center text-3xl text-white font-semibold mb-12 border-b-4 border-white py-2">
+            Colaboradores
+          </h1>
+          <div className="flex flex-wrap justify-around gap-2">
+            <CardProfile
+              foto="/images/foto_davi.jpg"
+              nome="Davi Salomão"
+              funcao="UX-UI"
+            />
+            <CardProfile
+              foto="/images/foto_Douglas.jpg"
+              nome="Douglas Santos"
+              funcao="Front-End Developer"
+            />
+            <CardProfile
+              foto=""
+              nome="Kauã Morais"
+              funcao="Back-End Developer"
+            />
+            <CardProfile
+              foto="/images/foto_guilherme.jpg"
+              nome="Guilherme Marques"
+              funcao="Data Base Manager"
+            />
+            <CardProfile
+              foto=""
+              nome="Larissa Alves"
+              funcao="Data Base Manager"
+            />
+          </div>
         </div>
       </section>
     </>
